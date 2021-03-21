893f8528cb259251d44f03cfdf83a2bcaff93f8e
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-03-21 22:31:57 +0530

    1. add auth

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,18 @@
-exports.sayHi = (req,res)=>{
-    res.json({message : "Hi from controller"})
+const User = require("../models/user")
+const {errorHandler} = require("../helpers/dbErrorHandler")
+
+exports.signup = (req,res)=>{
+   const user = new User(req.body)
+   user.save((err,user)=>{
+       if(err){
+            return res.status(400).json(
+                {
+                    err : errorHandler(err)
+                })
+       }
+       //hiding fields in response
+        user.salt = undefined
+        user.hashed_password = undefined
+        res.json({user})
+   })
 }
