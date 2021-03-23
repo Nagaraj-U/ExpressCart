5cc41797ccead362f754bd79dade28a9b7d6f6d7
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-03-23 21:54:03 +0530

    Delete user.js

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-//controllers
-const {signup} = require("../controllers/user")
-
-router.post("/signup",signup)
-
-module.exports = router
