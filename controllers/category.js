f1263a012a59b259cb1bebb5b0734098dbd74b0f
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-03-27 22:32:50 +0530

    CRUD for product and categories

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,6 @@
 const Category = require("../models/category")
 const {errorHandler} = require("../helpers/dbErrorHandler")
+const category = require("../models/category")
 
 
 exports.create = (req,res)=>{
@@ -12,5 +13,64 @@ exports.create = (req,res)=>{
         }
 
         res.json({data})
+    })
+}
+
+exports.categoryById = (req,res,next,id)=>{
+    Category.findById(id).exec((err,category)=>{
+        if(err){
+            return res.status(400).json({
+                error : "No such category exist"
+            })
+        }
+        req.category = category
+        next()
+    })
+}
+
+
+exports.read = (req,res)=>{
+    return res.json(req.category)
+}
+
+exports.list = (req,res)=>{
+    Category.find().exec((err,categories)=>{
+        if(err){
+            return res.status(400).json({
+                error : errorHandler(err)
+            })
+        }
+
+        res.json(categories)
+    })
+}
+
+exports.update = (req,res)=>{
+    let category = req.category //getting category which is already created
+    category.name = req.body.name //setting new name 
+    category.save((err,category)=>{
+        if(err){
+            return res.status(400).json({
+                error : errorHandler(err)
+            })
+        }
+
+        res.json(category)
+    })
+}
+
+exports.remove = (req,res)=>{
+
+    let category = req.category
+    category.remove((err,data)=>{
+        if(err){
+            return res.status(400).json({
+                error : errorHandler(err)
+            })
+        }
+
+        res.json({
+            "message" : "category deleted successfully !"
+        })
     })
 }
--- controllers/product.js
+++ controllers/product.js
@@ -18,8 +18,26 @@ exports.create = (req,res) => {
 
         let product = new Product(fields)
 
+        //checking all fields are available in form
+        const {name,description,price,quantity,category,shipping} = fields
+        if(!name || !category || !price || !quantity || !description || !shipping){
+            return res.status(400).json({
+                error : "All fields all mandatory"
+            })
+        }
+
+
         //checking if images present in form
-        if(files.photo){ //images -> files ("photo" : name under which client sends photo )
+        //1kb = 1000bytes -> 1mb = 1000*1000
+        if(files.photo){ //files (contain images or pdf etc) ("photo" : name under which client sends photo )
+            // console.log(`file size  ${files.photo.size} `);
+
+            if(files.photo.size > 1000000){
+                return res.status(400).json({
+                    error : "Image should be less that 1 mb"
+                })
+            }
+            
             product.photo.data = fs.readFileSync(files.photo.path) //schema fields
             product.photo.contentType = files.photo.type
         }
@@ -36,4 +54,83 @@ exports.create = (req,res) => {
     })
 
     
+}
+
+exports.productById = (req,res,next,id)=>{ //takes 4 paramaters
+    Product.findById(id).exec((err,product)=>{
+        if(err || !product){
+            return res.status(400).json({
+                error : "No such product found"
+            })
+        }
+
+        req.product = product
+        next()
+    })
+}
+
+exports.read = (req,res)=>{
+    req.product.photo = undefined
+    return res.json(req.product)
+}
+
+exports.remove = (req,res)=>{
+    let product = req.product
+    product.remove((err,deletedProduct)=>{
+        if(err){
+            return res.status(400).json({
+                error : errorHandler(err)
+            })
+        }
+
+        res.json({
+            "message" : "product deleted successfully"
+        })
+    })
+}
+
+exports.update = (req,res) =>{
+    //using formidable instead of req.body to handle image files
+    let form = new formidable.IncomingForm() 
+    form.keepExtensions = true
+    form.parse(req,(err,fields,files)=>{
+        if(err){
+            return res.status(400).json({
+                error : errorHandler(err) 
+            })
+        }
+
+        let product = req.product
+        product = _.extend(product,fields) //lodash library inbuilt func to update
+
+        //checking all fields are available in form
+        const {name,description,price,quantity,category,shipping} = fields
+        if(!name || !category || !price || !quantity || !description || !shipping){
+            return res.status(400).json({
+                error : "All fields all mandatory"
+            })
+        }
+
+        if(files.photo){ 
+
+            if(files.photo.size > 1000000){
+                return res.status(400).json({
+                    error : "Image should be less that 1 mb"
+                })
+            }
+            
+            product.photo.data = fs.readFileSync(files.photo.path) //schema fields
+            product.photo.contentType = files.photo.type
+        }
+
+        product.save((err,result)=>{
+            if(err){
+                return res.status(400).json({
+                    error : errorHandler(err)
+                })
+            }
+
+            res.json({result})
+        })
+    })
 }
