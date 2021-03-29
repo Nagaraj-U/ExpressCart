b5e1119c208fd63e440f2411eb85d1c7c5d933e9
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-03-29 22:58:50 +0530

    add product filters and user profile update

--- controllers/product.js
+++ controllers/product.js
@@ -134,3 +134,138 @@ exports.update = (req,res) =>{
         })
     })
 }
+
+
+
+/**
+getting product based on highest sold or new arrival 
+
+by sell : /products?sortBy=sold&order=desc&limit=4
+
+by arrival : /products?sortBy=created_at&order=desc&limit=4
+
+if no specific params mentioned then return all products by default
+
+**/
+exports.list = (req,res)=>{
+    let order = req.query.order ? req.query.order : "desc"
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id"
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+
+    Product.find()
+                  .select("-photo")
+                  .populate("category")
+                  .sort([[sortBy,order]])
+                  .limit(limit)
+
+    .exec((err,products)=>{
+        if(err){
+            return res.status(400).json({
+                error : "No product found"
+            })
+        }
+
+        res.json(products)
+    })
+}
+
+
+/*
+extracting all products which have have category of product in query
+excluding current product
+$ne : not equal
+find by id follwed by category
+*/
+exports.listRelated = (req,res)=>{
+    let limit = req.query.limit ? req.query.limit : 6
+
+    Product.find({_id : {$ne : req.product} , category : req.product.category})
+                .limit(limit)
+                .populate("category","_id name") //extracting id and name
+                .exec((err,products)=>{
+                    if(err){
+                        return res.status(400).json({
+                            error : "No such products found"
+                        })
+                    }
+
+                    res.json(products)
+                })
+}
+
+
+//return all product distinct categories
+exports.listCategories = (req,res)=>{
+    
+    Product.distinct("category").exec((err,categories) =>{
+        if(err){
+            return res.status(400).json({
+                error : "No such categories found" 
+            })
+        }
+        res.json(categories)
+    })
+}
+
+
+/**
+ * list products by search
+ * we will implement product search in react frontend
+ * we will show categories in checkbox and price range in radio buttons
+ * as the user clicks on those checkbox and radio buttons
+ * we will make api request and show the products to users based on what he wants
+ */
+
+exports.listBySearch = (req, res) => {
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
+
+    // console.log(order, sortBy, limit, skip, req.body.filters);
+    // console.log("findArgs", findArgs);
+
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                // gte -  greater than price [0-10]
+                // lte - less than
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+
+    Product.find(findArgs)
+        .select("-photo")
+        .populate("category")
+        .sort([[sortBy, order]])
+        .skip(skip)
+        .limit(limit)
+        .exec((err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Products not found"
+                });
+            }
+            res.json({
+                size: data.length,
+                data
+            });
+        });
+};
+
+
+//getting photo (in binary form ) ans setting to .jpg or .png according to req
+exports.photo = (req,res,next)=>{
+    if(req.product.photo.data){
+        res.set('Content-Type',req.product.photo.contentType)
+        return res.send(req.product.photo.data)
+    }
+    next()
+}
