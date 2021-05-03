ee8ecd0b8180d29888476099b5b29d83eef3c315
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-05-03 23:55:56 +0530

    add orders and payment routes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const userRoutes = require("./routes/user")
 const categoryRoutes = require("./routes/category")
 const productRoutes = require("./routes/product")
 const braintreeRoutes = require("./routes/braintree")
+const orderRoutes = require("./routes/order")
 
 
 //app
@@ -40,6 +41,7 @@ app.use("/api",userRoutes);
 app.use("/api",categoryRoutes)
 app.use("/api",productRoutes)
 app.use("/api",braintreeRoutes)
+app.use("/api",orderRoutes)
 
 
 app.listen(port,(req,res)=>{
