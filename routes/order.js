c8d237d666f6b84f56e8e686a1c617ad295856a2
Nagaraj U <nagarajullegaddi01@gmail.com>
2021-05-18 17:15:30 +0530

    admin manage route

--- routes/order.js
+++ routes/order.js
@@ -3,11 +3,14 @@ const router = express.Router()
 
 const {requireSignin,isAuth, isAdmin} = require("../controllers/auth")
 const {userById,addOrderToPurchaseHistory} = require("../controllers/user")
-const {create,listOrders} = require("../controllers/order")
+const {create,listOrders,getStatusValues,updateStatus,orderById} = require("../controllers/order")
 const {increaseCountDecreaseQuantity} = require("../controllers/product")
 
 router.post("/order/create/:userId",requireSignin,isAuth,addOrderToPurchaseHistory,increaseCountDecreaseQuantity,create)
 router.get("/order/list/:userId",requireSignin,isAuth,isAdmin,listOrders)
+router.get("/order/status-values/:userId",requireSignin,isAuth,isAdmin,getStatusValues)
+router.put("/order/:orderId/status/:userId",requireSignin,isAuth,isAdmin,updateStatus)
 router.param("userId",userById)
+router.param("orderId",orderById)
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const {userById,read,update} = require("../controllers/user")
+const {userById,read,update,userOrders} = require("../controllers/user")
 const {requireSignin,isAuth,isAdmin} = require("../controllers/auth")
 
 
@@ -10,14 +10,9 @@ router.param("userId",userById); //grabs userid from route params (used in displ
 
 //authenticated user ,admin , signed in users can only access
 
-// router.get("/user/:userId",requireSignin,isAuth,isAdmin,(req,res)=>{
-//     res.json({
-//         user : req.profile
-//     })
-// })
-
 router.get("/user/:userId",requireSignin,isAuth,read);
 router.put("/user/:userId",requireSignin,isAuth,update);
+router.get("/orders/by/user/:userId",requireSignin,isAuth,userOrders);
 
 
 module.exports = router
